fda44de6e14f93c873241f506adcbc42765326c2
2158YarosGol2158 <ga5293612@gmail.com>
2025-03-14 17:05:37 +0200

    mfff

--- main1.py
+++ main1.py
@@ -1,5 +1,18 @@
 def ocinka(a):
-    if a > "....":
-        return "незадовільно"
-    elif a > "....":
+    if a > 89:
+        return "відмінно"
+    elif a > 69:
+        return "добре"
+    elif a > 49:
         return "задовільно"
+    elif a > -1:
+        return "незадовільно"
+    else:
+        return "а що тут введено"
+while True:
+    try:
+        d = int(input("Введіть оцінку"))
+    except ValueError:
+        print("що це?")
+    else:
+        print(ocinka(d))
