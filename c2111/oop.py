53439f797e489801c5da87053e12ab459310c3d7
2158YarosGol2158 <ga5293612@gmail.com>
2025-03-21 19:39:57 +0200

    вже домашня робота

--- c2111/oop.py
+++ c2111/oop.py
@@ -38,26 +38,36 @@ import random as r
 class Student:
     def __init__(self,name):
         self.name=name
-        self.happy=r.randint(10,100)
+        self.happy=r.randint(40,100)
         self.progres=r.randint(0,10)
         self.alive=True
+        self.money=100
+        self.expelled=False
+        self.ifsleep=True
     def study(self):
         print("Час для навчання")
-        self.happy-=r.randint(1,5)
-        self.progres+=r.randint(1,10)
+        self.happy-=r.randint(10,15)
+        self.progres+=r.randint(3,15)
+        self.ifsleep = False
     def sleep(self):
         print("Час для сну")
         self.happy+=r.randint(1, 5)
+        self.progres-=r.randint(1,4)
+        self.ifsleep=True
     def chill(self):
         print("Час для відпочинку")
         self.happy+=r.randint(50,100)
         self.progres-=r.randint(1, 10)
+        self.money-=r.randint(200,1000)
+        self.ifsleep = False
     def isAlive(self):
         if 1<self.progres<5:
             print("Ти на грані відрахувавння. Йди, навчайся")
             self.alive=False
         elif self.progres<=1:
+
             self.alive=False
+            self.expelled=True
             print("ВІДРАХОВАНИЙ!")
         elif self.progres>=5:
             print("Молодець")
@@ -65,23 +75,32 @@ class Student:
     def everyday(self):
         print("Рівень щастя: ", self.happy)
         print("Прогрес навчання: ", self.progres)
+        print("Гроші: ", self.money)
     def studyLove(self,day):
         day="\nДень №"+str(day)
         print(day)
-        res=r.randint(1,3)
-        if res == 1:
+        if self.alive == False or self.progres <= 3:
             self.study()
-        elif res == 2:
-            self.sleep()
-        elif res == 3:
+        elif self.money <= 25:
+            self.work()
+        elif self.happy <= 25:
             self.chill()
+        elif self.ifsleep == False:
+            self.sleep()
+        else:
+            self.study()
         self.everyday()
         self.isAlive()
+    def work(self):
+        print("Час для роботи")
+        self.money+=r.randint(50,3500)
+        self.progres-=r.randint(1,10)
+        self.ifsleep = False
 
 st1=Student("Олег")
 # print(st1.progres)
 print("Життя студента",st1.name)
-for c in range(1,8):
-    if st1.alive==False:
+for c in range(1,366):
+    if st1.expelled==True:
         break
     st1.studyLove(c)
