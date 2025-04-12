476858bed1957a045c475b630700c0a4a0cb245c
2158YarosGol2158 <ga5293612@gmail.com>
2025-04-12 14:55:59 +0300

    Контрольна робота

--- c2111/req.py
+++ c2111/req.py
@@ -194,6 +194,7 @@ else:
     print("Жодної інформації не отримано")
 """
 
+"""
 from http.client import responses
 
 import requests
@@ -241,5 +242,84 @@ obj.auditSite()
 site=obj.getInfo()
 if site:
     obj.showInfo(site)
+else:
+    print("Жодної інформації не отримано")
+"""
+import requests
+from bs4 import BeautifulSoup as bs
+
+
+class Eva:
+    def __init__(self,url):
+        self.url=url
+        self.header={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        }
+        self.soup=None
+    def auditSite(self):
+        response=requests.get(self.url,headers=self.header)
+        if response.status_code==200:
+            response.encoding = 'utf-8'
+            self.soup=bs(response.text,"html.parser")
+        else:
+            print("Не вдалося підєднатися до сайту")
+
+    def getInfo(self):
+            cosmetic = []
+            cosm = self.soup.find_all('div', class_='product')
+            if not cosm:
+                print('Помилка в пошуку на сторінці')
+                return cosmetic
+            for i in cosm[:10]:
+                name = i.find('p', class_='product__title').text.strip()
+                price = i.find('span', class_='product__regular-price').text.strip()
+                cosmetic.append({
+                    'Назва':name,
+                    'Ціна':price
+                })
+            return cosmetic
+
+    def showInfo(self, cosmetic):
+        print('Топ-10 товарів:')
+        n = 1
+        for cosmetic_c in cosmetic:
+            print(f"{n}. {cosmetic_c['Назва']} - {cosmetic_c['Ціна']}")
+            n += 1
+        return cosmetic
+
+    def cofigurator(self,cosmetic):
+        cart = []
+        while True:
+            curent_tovar=input("Який товар ви хочете придбати? (введіть номер): ")
+            scilcu=input("Скільки одиниць ви хочете купити? ")
+            he=input("Хочете ще щось? (так/ні): ")
+
+            cart.append({
+                "Товар":curent_tovar,
+                "Кількість":scilcu
+            })
+            if he == "ні":
+                return cart,cosmetic
+
+    def calculator(self,cart,cosmetic):
+        print("Ваше замовлення:")
+        n = 0
+        price = 0
+        for i in cart:
+            print(f" - {cosmetic[int(cart[n]['Товар'])]['Назва']} x{cart[n]['Кількість']} = {int(''.join(ch for ch in cosmetic[int(cart[n]['Товар'])]['Ціна'] if ch.isdigit())) * int(cart[n]['Кількість'])} грн")
+            price+=int(''.join(ch for ch in cosmetic[int(cart[n]['Товар'])]['Ціна'] if ch.isdigit())) * int(cart[n]['Кількість'])
+            n+=1
+        print("Загальна сума до сплати:", str(price), "грн")
+
+
+
+url="https://eva.ua/ua/13557/eva-premium/"
+obj=Eva(url)
+obj.auditSite()
+site=obj.getInfo()
+if site:
+    s = obj.showInfo(site)
+    f = obj.cofigurator(s)
+    obj.calculator(f[0],f[1])
 else:
     print("Жодної інформації не отримано")
